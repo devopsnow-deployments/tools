158f7417b323d5667b0f432c69c7716f6a2252c9
devopsnow-admin <81858097+devopsnow-admin@users.noreply.github.com>
2021-04-03 07:05:34 -0700

    Initial working version

--- scripts/remote-cluster-bootstrap.sh
+++ scripts/remote-cluster-bootstrap.sh
@@ -1,1 +1,92 @@
-echo "Bootstrap your remote cluster with DevOpsNow"
+#! /bin/bash
+
+# Read all command line arguments
+for ARGUMENT in "$@"
+do
+
+    KEY=$(echo $ARGUMENT | cut -f1 -d=)
+    VALUE=$(echo $ARGUMENT | cut -f2 -d=)   
+
+    case "$KEY" in
+            cluster_name)              cluster_name=${VALUE} ;;
+            namespace)    namespace=${VALUE} ;;   
+            devopsnow_repo_username)    devopsnow_repo_username=${VALUE} ;;
+            devopsnow_repo_password)    devopsnow_repo_password=${VALUE} ;;
+            devopsnow_application_sourceRepoURL)    devopsnow_application_sourceRepoURL=${VALUE} ;;
+            devopsnow_registry_username)    devopsnow_registry_username=${VALUE} ;;
+            devopsnow_registry_password)    devopsnow_registry_password=${VALUE} ;;
+            *)   
+    esac    
+done
+
+# TODO
+# print  help if needed
+
+# Setup some derived variables
+devopsnow_application_sourceRepoPath="remote/$cluster_name/apps"
+
+# Testing
+echo "cluster_name = $cluster_name"
+echo "namespace = $namespace"
+echo "devopsnow_repo_username = $devopsnow_repo_username"
+echo "devopsnow_repo_password = $devopsnow_repo_password"
+echo "devopsnow_application_sourceRepoURL = $devopsnow_application_sourceRepoURL"
+echo "devopsnow_application_sourceRepoPath = $devopsnow_application_sourceRepoPath"
+echo "devopsnow_registry_username = $devopsnow_registry_username"
+echo "devopsnow_registry_password = $devopsnow_registry_password"
+
+# Validate that all required inputs are provided
+echo ""
+echo "Validating input arguments ..."
+if [[ -n $cluster_name ]] \
+    && [[ -n $namespace ]] \
+    && [[ -n $devopsnow_repo_username ]] \
+    && [[ -n $devopsnow_repo_password ]] \
+    && [[ -n $devopsnow_application_sourceRepoURL ]] \
+    && [[ -n $devopsnow_application_sourceRepoPath ]] \
+    && [[ -n $devopsnow_registry_username ]] \
+    && [[ -n $devopsnow_registry_password ]];
+then
+    echo "All required arguments are present. Continuing ..."
+else
+    echo "Not all required arguments are present. The following arguments are required: "
+    echo "  cluster_name"
+    echo "  namespace"
+    echo "  devopsnow_repo_username"
+    echo "  devopsnow_repo_password"
+    echo "  devopsnow_application_sourceRepoURL"
+    echo "  devopsnow_application_sourceRepoPath"
+    echo "  devopsnow_registry_username"
+    echo "  devopsnow_registry_password"
+fi
+
+# Validate if kubectl and helm are available
+command -v "kubectl" >/dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+    echo "kubectl is not found. Please check if it is installed."
+    echo "Exiting ..."
+    exit 1
+fi
+
+command -v "helm" >/dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+    echo "helm is not found. Please check if it is installed."
+    echo "Exiting ..."    
+    exit 1
+fi
+
+echo ""
+echo "Installing the bootstrap components to the namespace $namespace ..."
+helm install remote-bootstrap-now -n $namespace --create-namespace ~/git/charts/src/remote-bootstrap/ \
+  --set devopsnow.repo.username=$devopsnow_repo_username \
+  --set devopsnow.repo.password=$devopsnow_repo_username \
+  --set devopsnow.application.sourceRepoURL=devopsnow_application_sourceRepoURL \
+  --set devopsnow.application.sourceRepoPath=devopsnow_application_sourceRepoPath
+
+echo ""
+echo "Waiting for sealed-secrets component to create the key pair ..."
+sleep 60
+
+echo "Please send the following public key (base64 encoded) back to DevOpsNow ..."
+echo ""
+echo `kubectl get secret -n ${namespace} -l 'sealedsecrets.bitnami.com/sealed-secrets-key=active' -o jsonpath='{.items[].data.tls\.crt}'`

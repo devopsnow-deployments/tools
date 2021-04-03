ae97117c3ecf4e0fe47d50eecd5afb78cb0ca846
devopsnow-admin <81858097+devopsnow-admin@users.noreply.github.com>
2021-04-03 05:18:08 -0700

    Update remote-cluster-bootstrap.sh

--- scripts/remote-cluster-bootstrap.sh
+++ scripts/remote-cluster-bootstrap.sh
@@ -1,1 +1,17 @@
-echo "Bootstrap your remote cluster with DevOpsNow $name"
+for ARGUMENT in "$@"
+do
+
+    KEY=$(echo $ARGUMENT | cut -f1 -d=)
+    VALUE=$(echo $ARGUMENT | cut -f2 -d=)   
+
+    case "$KEY" in
+            CLUSTER_NAME)              CLUSTER_NAME=${VALUE} ;;
+            NAMESPACE)    NAMESPACE=${VALUE} ;;     
+            *)   
+    esac    
+
+
+done
+
+echo "CLUSTER_NAME = $CLUSTER_NAME"
+echo "NAMESPACE = $NAMESPACE"
